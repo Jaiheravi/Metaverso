d76c2a12464fe0d5d8869b97340df797346ad39a
Jaime Hernández <contact@jaime.one>
2020-12-18 22:15:31 -0700

    Remove modeline

--- init.el
+++ init.el
@@ -107,12 +107,20 @@
 ;; Theme
 (use-package doom-themes
   :config
-  (load-theme 'doom-one t))
+  (load-theme 'doom-one t)
+  (set-face-background 'region "black"))
 
-(use-package doom-modeline
-  :init (doom-modeline-init)
-  :config
-  (setq doom-modeline-icon nil))
+(use-package feebleline
+  :ensure t
+  :config (setq feebleline-msg-functions
+                '((feebleline-line-number         :post "" :fmt "%5s")
+                  (feebleline-column-number       :pre ":" :fmt "%-2s")
+                  (feebleline-file-directory      :face feebleline-dir-face :post "")
+                  (feebleline-file-or-buffer-name :face font-lock-keyword-face :post "")
+                  (feebleline-file-modified-star  :face font-lock-warning-face :post "")
+                  (feebleline-git-branch          :face feebleline-git-face :pre " : ")
+                  (feebleline-project-name        :align right)))
+  (feebleline-mode 1))
 
 ;; Highlight known Emacs Lisp symbols
 (use-package highlight-defined
